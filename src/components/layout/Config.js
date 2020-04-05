1977cc4009347d1ea75a5427e8f339608a3c144f
Shoji Makino <pitang1965@gmail.com>
2020-04-05 20:20:56 +0900

    下3つのスライダーの刻みを細かくした

--- src/components/layout/Config.js
+++ src/components/layout/Config.js
@@ -7,9 +7,9 @@ const Config = () => {
   return (
     <div className='config'>
       <Slider parameterTitle={'顔の大きさ'} onChange={(_, value) => configContext.setFaceSize(value)} min={96} max={512} value={configContext.faceSize} />
-      <Slider parameterTitle={'目の大きさ'} onChange={(_, value) => configContext.setEyeSize(value)} min={0.7} max={1.0} step={0.1} value={configContext.eyeSize} />
+      <Slider parameterTitle={'目の大きさ'} onChange={(_, value) => configContext.setEyeSize(value)} min={0.7} max={1.0} step={0.02} value={configContext.eyeSize} />
       <Slider parameterTitle={'口の大きさ'} onChange={(_, value) => configContext.setMouseSize(value)} min={0.6} max={1.2} step={0.02} value={configContext.mouseSize} />
-      <Slider parameterTitle={'ほっぺの大きさ'} onChange={(_, value) => configContext.setCheekSize(value)} min={0.7} max={1.3} step={0.1} value={configContext.cheekSize} />
+      <Slider parameterTitle={'ほっぺの大きさ'} onChange={(_, value) => configContext.setCheekSize(value)} min={0.7} max={1.3} step={0.02} value={configContext.cheekSize} />
     </div>
   );
 };
