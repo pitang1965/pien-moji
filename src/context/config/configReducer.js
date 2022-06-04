e89d9d4c530ecdbbdc75b1e853577d0a9a83aaef
Shoji Makino <pitang1965@gmail.com>
2022-06-04 20:42:56 +0900

    Fix: import / no-anonymous-default-export

--- src/context/config/configReducer.js
+++ src/context/config/configReducer.js
@@ -1,28 +1,33 @@
-import { SET_FACE_SIZE, SET_EYE_SIZE, SET_MOUSE_SIZE, SET_CHEEK_SIZE } from '../types';
+import {
+  SET_FACE_SIZE,
+  SET_EYE_SIZE,
+  SET_MOUSE_SIZE,
+  SET_CHEEK_SIZE,
+} from '../types';
 
-export default (state, action) => {
+export default function configReducer(state, action) {
   switch (action.type) {
     case SET_FACE_SIZE:
       return {
         ...state,
-        faceSize: action.payload
-      }
-      case SET_EYE_SIZE:
-        return {
-          ...state,
-          eyeSize: action.payload
-      }
-      case SET_MOUSE_SIZE:
-        return {
-          ...state,
-          mouseSize: action.payload
-      }
-      case SET_CHEEK_SIZE:
-        return {
-          ...state,
-          cheekSize: action.payload
-        }
+        faceSize: action.payload,
+      };
+    case SET_EYE_SIZE:
+      return {
+        ...state,
+        eyeSize: action.payload,
+      };
+    case SET_MOUSE_SIZE:
+      return {
+        ...state,
+        mouseSize: action.payload,
+      };
+    case SET_CHEEK_SIZE:
+      return {
+        ...state,
+        cheekSize: action.payload,
+      };
     default:
       return state;
   }
-};
+}
